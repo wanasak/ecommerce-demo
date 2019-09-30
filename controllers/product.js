02e66d6d1eb13ed9c5ba97c7b4f3cd161a5aac30
Wanasak Suraintaranggoon <u510610433@gmail.com>
2019-09-30 17:09:42 +0700

    search product with queries

--- controllers/product.js
+++ controllers/product.js
@@ -128,3 +128,103 @@ exports.update = (req, res) => {
     });
   });
 };
+
+/**
+ * sell / arrival
+ * by sell = /products?sortBy=sold&order=desc&limit=4
+ * by arrival = /products?sortBy=createdAt&order=desc&limit=4
+ */
+exports.list = (req, res) => {
+  let order = req.query.order || 'asc';
+  let sortBy = req.query.sortBy || '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+  Product.find()
+    .select('-photo')
+    .populate('category')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, products) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Products not found'
+        });
+      }
+      res.json(products);
+    });
+};
+
+/**
+ * it will find the products based on the req product category
+ */
+exports.listRelated = (req, res) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+  // $ne = not equal
+  Product.find({
+    _id: { $ne: req.product },
+    category: req.product.category
+  })
+    .limit(limit)
+    .populate('category')
+    .exec((err, products) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Products not found'
+        });
+      }
+      res.json(products);
+    });
+};
+
+exports.listCategories = (req, res) => {
+  Product.distinct('category', {}, (err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Categories not found'
+      });
+    }
+    res.json(categories);
+  });
+};
+
+exports.listBySearch = (req, res) => {
+  let order = req.body.order || 'desc';
+  let sortBy = req.body.sortBy || '_id';
+  let limit = req.body.limit ? parseInt(req.body.order) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {};
+
+  for (const key in req.body.filters) {
+    if (req.body.filters[key].length > 0) {
+      if (key === 'price') {
+        // $gte - greater than price [0-10]
+        // $lte - lower than
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1]
+        };
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  Product.find(findArgs)
+    .select('-photo')
+    .populate('category')
+    .sort([[sortBy, order]])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Products not found'
+        });
+      }
+      res.json({
+        size: data.length,
+        data
+      });
+    });
+};
--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,11 @@ const {
   productById,
   read,
   remove,
-  update
+  update,
+  list,
+  listRelated,
+  listCategories,
+  listBySearch
 } = require('../controllers/product');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
@@ -27,6 +31,10 @@ router.put(
   isAuth,
   update
 );
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.get('/products/by/search', listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
