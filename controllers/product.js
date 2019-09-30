9a93ab86e6ee9ed4b5ec4335d323b2f562993678
Wanasak Suraintaranggoon <u510610433@gmail.com>
2019-09-30 14:42:05 +0700

    product and category

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,26 @@
 const formidable = require('formidable');
 const _ = require('lodash');
+const fs = require('fs');
 
 const Product = require('../models/product');
+const { errorHandler } = '../helpers/dbErrorHandler';
+
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
@@ -12,9 +31,100 @@ exports.create = (req, res) => {
         error: 'Image could not be upload'
       });
     }
+    const { name, description, price, category, shipping, quantity } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !shipping ||
+      !quantity
+    ) {
+      return res.status(400).json({
+        error: 'All fileds are required'
+      });
+    }
+
     let product = new Product(fields);
 
     if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb'
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
     }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove(err => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: 'Product deleted successfully'
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not be upload'
+      });
+    }
+    const { name, description, price, category, shipping, quantity } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !shipping ||
+      !quantity
+    ) {
+      return res.status(400).json({
+        error: 'All fileds are required'
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb'
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
   });
 };
